879d5edd68dbd168a6b4e76f61c9e57f444e4a5a
verghesetejas <verghesetejas@gmail.com>
2021-08-19 18:00:22 +0530

    This is a React Sample App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  badgeStyle = {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 10,
+  };
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <span style={ this.badgeStyle } className="badge badge-primary m-2">{ this.state.count }</span>
+        <button className="btn btn-secondary btn-sm" onClick={this.handleIncrement}>Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  add = (() => {
+    let counter = 0;
+    return () => {
+      return counter++;
+    };
+  })();
+
+  handleIncrement() {
+    this.setState({ count: this.add() });
+  }
 }
 
 export default App;
